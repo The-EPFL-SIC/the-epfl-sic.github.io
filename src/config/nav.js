173921d1ba0abac0621a72bf65409c8509bd5718
Killian <killian.hd@gmail.com>
2024-10-14 22:50:41 +0200

    1st deploy test

---
+++ src/config/nav.js
@@ -0,0 +1,14 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About Us',
+		slug: '/asso/about',
+	},
+	{
+		title: 'Join Us',
+		slug: 'https://docs.google.com/forms/d/e/1FAIpQLSeSR8U3L16fpSPG1i2OKlz2MVtnFDUylCiH7QcfVcmJ1rnt4A/viewform?usp=sf_link'
+	}
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `EPFL Sustainable Innovation Competition`, // The title of the website
+	description: `A competition for students to develop sustainable innovations.`, // The description of the website
+	url: `https://the-epfl-sic.github.io`, // No trailing slash!
+	name: `EPFL-SIC`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
