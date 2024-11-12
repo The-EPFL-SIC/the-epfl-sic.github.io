3f30dd05d54d0a05ae1b0936341e6578af485d6d
Killian <killian.hd@gmail.com>
2024-11-12 18:41:48 +0100

    Fixed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://the-epfl-sic.github.io/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://epfl-sic.ch/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
